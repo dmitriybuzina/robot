064f9326f23654bf4b919f947a717dff515c792c
dmitriybuzina <dmitry.buzina@faceit-team.com>
2018-09-27 14:45:00 +0300

    second

--- main.rb
+++ main.rb
@@ -1,46 +1,60 @@
-require_relative 'table'
 require_relative 'robot'
 
 class Main
-
   def start
     puts '1 - write size of table, else - default'
     if gets.chomp == '1'
-      puts 'Write height:'
-      height = gets.chomp.to_i
-      puts 'Write width:'
-      width = gets.chomp.to_i
-      @table = Table.new(height, width)
+      puts 'write height:'
+      @table_height = gets.chomp.to_i
+      puts 'write width:'
+      @table_width = gets.chomp.to_i
     else
-      @table = Table.new(5, 6)
+      @table_height = 5
+      @table_width = 6
     end
-    puts 'Write command'
-    command = gets.chomp.to_s
-    if command.include? 'PLACE'
-      x = command[6]
-      y = command[8]
-      f = command[10]
-      @robot = Robot.new(x,y,f)
-    else
-      case command.to_sym
-      when :MOVE
-
-      when :LEFT
-
-      when :RIGHT
-
-      when :REPORT
-      end
-    end
+    read
+    parse
   end
 
   def read
-    puts 'Write path of file'
+    puts 'write path of file'
     @array_commands = []
-    File.open(gets.chomp , 'r').each { |line| @array_commands << line }
-    res = 0
+    File.open(gets.chomp, 'r').each { |line| @array_commands << line }
+  end
+
+  def check
+    return false if @robot.x == @table_width.to_i - 1 && @robot.f == :EAST
+    return false if @robot.x.zero? && @robot.f == :WEST
+    return false if @robot.y.to_i == @table_height - 1 && @robot.f == :NORTH
+    return false if @robot.y.zero? && @robot.f == :SOUTH
+
+    true
+  end
+
+  def parse
+    flag = false
+    @array_commands.each_index do |i|
+      if @array_commands[i].include? 'PLACE'
+        item = @array_commands[i].split(/\W+/)
+        if item[1].to_i < @table_width && item[2].to_i < @table_height && item[1].to_i >= 0 && item[2].to_i >= 0
+          @robot = Robot.new(item[1].to_i, item[2].to_i, item[3].to_sym)
+          flag = true
+        end
+      elsif flag
+        case @array_commands[i].chomp.to_sym
+        when :MOVE
+          @robot.move if check
+        when :RIGHT
+          @robot.rotation :RIGHT
+        when :LEFT
+          @robot.rotation :LEFT
+        when :REPORT
+          @robot.report
+        end
+      end
+    end
   end
 end
 
 main = Main.new
-main.read
+main.start
--- robot.rb
+++ robot.rb
@@ -1,9 +1,8 @@
-require_relative 'table'
-
 class Robot
-  @x
-  @y
-  @f
+  @@look = [:NORTH, :EAST, :SOUTH, :WEST]
+  attr_accessor :x,
+                :y,
+                :f
 
   def initialize(x, y, f)
     @x = x
@@ -11,13 +10,29 @@ class Robot
     @f = f
   end
 
+  def report
+    puts "Robot position: #{@x}, #{@y}, #{@f} "
+  end
+
+  def rotation command
+    index = @@look.index(@f)
+    if command == :RIGHT
+      index != @@look.length - 1 ? @f = @@look[index + 1] : @f = @@look[0]
+    elsif command == :LEFT
+      index.zero? ?  @f = @@look[@@look.length - 1] : @f = @@look[index - 1]
+    end
+  end
+
   def move
     case @f
     when :NORTH
+      @y += 1
     when :EAST
+      @x += 1
     when :SOUTH
+      @y -= 1
     when :WEST
-
+      @x -= 1
     end
   end
 end
